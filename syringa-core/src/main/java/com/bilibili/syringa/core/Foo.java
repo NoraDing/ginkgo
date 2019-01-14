c32065740ad9c3231f836f24a86dafa12c08ac4c
薛赵明 <xuezhaoming@xuezhaomingdeMacBook-Pro.local>
2019-01-14 11:09:33 +0800

    init project

---
+++ syringa-core/src/main/java/com/bilibili/syringa/core/Foo.java
@@ -0,0 +1,12 @@
+/**
+ * Bilibili.com Inc. Copyright (c) 2009-2019 All Rights Reserved.
+ */
+package com.bilibili.syringa.core;
+
+/**
+ *
+ * @author xuezhaoming
+ * @version $Id: Foo.java, v 0.1 2019-01-14 11:05 AM Exp $$
+ */
+public class Foo {
+}
