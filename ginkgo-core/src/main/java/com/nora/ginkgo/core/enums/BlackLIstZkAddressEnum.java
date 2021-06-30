89fd48c9dfe0a6f82411fa8c1c031afc63f3e661
dingsainan <dingsainan@bilibili.com>
2021-06-30 14:15:07 +0800

    update

--- ginkgo-core/src/main/java/com/nora/ginkgo/core/enums/BlackLIstZkAddressEnum.java
+++ ginkgo-core/src/main/java/com/nora/ginkgo/core/enums/BlackLIstZkAddressEnum.java
@@ -3,8 +3,7 @@ package com.nora.ginkgo.core.enums;
 import com.google.common.base.Preconditions;
 
 public enum BlackLIstZkAddressEnum {
-//    KAFKA_BLACKLIST_ZOOKEEPER_ADDRESS("kafka.blacklist.zookeeper.address", "10.70.141.19:2181,10.70.141.20:2181,10.70.141.21:2181");
-    KAFKA_BLACKLIST_ZOOKEEPER_ADDRESS("kafka.blacklist.zookeeper.address", "10.69.99.13:2181,10.69.99.14:2181,10.69.99.15:2181,10.69.99.16:2181,10.69.99.17:2181");
+     KAFKA_BLACKLIST_ZOOKEEPER_ADDRESS("kafka.blacklist.zookeeper.address", "XXXX");
     private String type;
     private String zkAddress;
 
